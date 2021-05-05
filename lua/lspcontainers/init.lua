7939ff505613d4ed5aa4b103728b05079a4761b7
Adrian Ordonez <adrianord@gmail.com>
2021-05-05 11:13:26 -0500

    feat(*): create table for supported languages and allow enduser to provide more languages

--- lua/lspcontainers/init.lua
+++ lua/lspcontainers/init.lua
@@ -1,20 +1,22 @@
+local supported_languages = {
+  sumneko_lua = "lspcontainers/lua-language-server:1.20.5",
+  dockerls = "lspcontainers/docker-langserver:0.4.1"
+}
+
 local function command(server, user_opts)
   local opts = user_opts or {}
+
   local workdir = opts.root_dir or vim.fn.getcwd()
-  local image = ""
   local volume = workdir..":"..workdir
 
-  -- TODO: dockerfile exists, needs implementation
-  --if server == "dockerls" then
-    --image = "lspcontainers/docker-langserver:0.4.1"
-  --end
+  local additional_languages = opts.additional_languages or {}
+  local image = additional_languages[server]
+                or supported_languages[server]
+                or nil
 
-  if server == "sumneko_lua" then
-    image = "lspcontainers/lua-language-server:1.20.5"
-  end
-
-  if image == "" then
-    error("Invalid language server provided")
+  if not image then
+    error(string.format("lspcontainers: language not supported `%s`", server))
+    return 1
   end
 
   return {
