13f07eb5ee6fed2646498420fee2f6c62c93c460
Erik R <e@drn.io>
2021-07-02 14:44:36 -0700

    fix(pylsp)

--- lua/lspcontainers/init.lua
+++ lua/lspcontainers/init.lua
@@ -18,7 +18,7 @@ local supported_languages = {
   jsonls = { image = "lspcontainers/json-language-server:1.3.4", cmd = default_cmd },
   gopls = { image = "lspcontainers/gopls:0.6.11", cmd = default_cmd },
   html = { image = "lspcontainers/html-language-server:1.4.0", cmd = default_cmd },
-  pylsp = { image = "lspcontainers/python-lsp:1.0.1", cmd = default_cmd },
+  pylsp = { image = "lspcontainers/python-lsp-server:1.1.0", cmd = default_cmd },
   pyright = { image = "lspcontainers/pyright-langserver:1.1.137", cmd = default_cmd },
   rust_analyzer = { image = "lspcontainers/rust-analyzer:2021-05-03", cmd = default_cmd },
   svelte = { image = "lspcontainers/svelte-language-server:0.13.7", cmd = default_cmd },
