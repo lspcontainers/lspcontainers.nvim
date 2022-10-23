865213ad9268dff7659b04ef08c04466f68e671e
Erik Reinert <erik@altf4.email>
2022-10-23 15:58:52 -0700

    feat(flake): added initial inputs and outputs (#90)

---
+++ test/init.lua
@@ -0,0 +1,96 @@
+local function on_attach(client, bufnr)
+    local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+
+    local opts = { noremap = true, silent = true }
+
+    buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+    buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
+    buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+    buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+    buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+    buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+    buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+    buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+    buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+    buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+    buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+    buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+    buf_set_keymap('n', '<space>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
+    buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
+    buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
+    buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+
+    if client.server_capabilities.documentFormattingProvider then
+        buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.format({ async = true })<CR>", opts)
+    end
+
+    if client.server_capabilities.documentRangeFormattingProvider then
+        buf_set_keymap("v", "<space>f", "<cmd>lua vim.lsp.buf.range_formatting()<CR>", opts)
+    end
+
+    if client.server_capabilities.documentHighlightProvider then
+        vim.api.nvim_exec([[
+          augroup lsp_document_highlight
+            autocmd! * <buffer>
+            autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
+            autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
+          augroup END
+        ]], false)
+    end
+end
+
+local function make_config()
+    local capabilities = vim.lsp.protocol.make_client_capabilities()
+
+    capabilities.textDocument.completion.completionItem.snippetSupport = true
+    capabilities.textDocument.completion.completionItem.resolveSupport = {
+        properties = {
+            'documentation',
+            'detail',
+            'additionalTextEdits',
+        }
+    }
+
+    return {
+        capabilities = capabilities,
+        on_attach = on_attach
+    }
+end
+
+local function setup_languages()
+    local lspcontainers_servers = {
+        "bashls",
+        "clangd",
+        "dockerls",
+        "gopls",
+        "graphql",
+        "html",
+        "intelephense",
+        "jsonls",
+        "omnisharp",
+        "powershell_es",
+        "prismals",
+        "pylsp",
+        "pyright",
+        "rust_analyzer",
+        "solargraph",
+        "sumneko_lua",
+        "svelte",
+        "tailwindcss",
+        "terraformls",
+        "tsserver",
+        "vuels",
+        "yamlls"
+    }
+
+    for _, server in pairs(lspcontainers_servers) do
+        local config = make_config()
+
+        config.cmd = require 'lspcontainers'.command(server)
+        config.root_dir = require 'lspconfig/util'.root_pattern(".git", vim.fn.getcwd())
+
+        require 'lspconfig'[server].setup(config)
+    end
+end
+
+setup_languages()
